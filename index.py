5fa5f08749feb761d262c8c2dfc30debc2dd3ae1
yuri31122006-eng <yuri31122006@gmail.com>
2026-09-13 21:23:42 -0300

    Função 1 e 2 comcluidas --O sistema ja põe produtos reduz estoque e mostra soma-- .

--- index.py
+++ index.py
@@ -9,63 +9,111 @@ try:
 except FileNotFoundError:
     produtos = []
 
+carrinho = []
+
 print("\033[33mSistema de Caixa\033[0m")
 print()
 sleep(0.6)
 
 print("[1] Adicionar produtos ao carrinho")
 print()
+print("[2] Comfirmar a compra !")
+print()
+
 
 while True:
 
-    escolha = input("Digite sua escolha: ")
-    print()
+    while True:
 
-    if escolha.isnumeric():
-        break
-    else:
-        print("\033[31mIsso não é um número !\033[0m")
+        escolha1 = input("Digite sua escolha: ")
         print()
 
-if escolha == 1:
+        if escolha1.isnumeric() == True:
+            break
+        else:
+            print("\033[31mIsso não é um número !\033[0m")
+            print()
 
-    carrinho = []
+    escolha = float(escolha1)
 
-    while True:
+    if escolha == 1:
+
+        
 
         while True:
 
             while True:
 
-                co_prodto =input("Digite o código do produto: ")
-                print()
+                while True:
 
-                if co_prodto.isnumeric():
-                    break
-                else:
-                    print("\033[31mIsso não é um número !\033[0m")
+                    co_produto =input("Digite o código do produto: ")
                     print()
 
-            produto_enco = None
+                    produto_enco = None
 
-            for produto in produtos:
-                if produto['codigo'] == co_prodto:
-                    produto_enco = produto
+                    for produto in produtos:
+                        if produto['codigo'] == co_produto:
+                            produto_enco = produto
+                            break
+
+                    if produto_enco is not None:
+                        break
+                    else:
+                        print("\033[31mProduto não encontrado !\033[0m")
+                        print()
+
+                print(f"Nome: {produto_enco['nome']}")
+                print(f"Preço: {produto_enco['preço']}")
+                print(f"Estoque: {produto_enco['estoque']}")
+                print(f"Código: {produto_enco['codigo']}")
+                print()
+
+                opção = input("O produto está correto : ").lower().strip()
+                print()
+
+                if opção in ['sim', 's', 'positivo', 'correto', 'certo', 'ss']:
+
+                    while True:
+
+                        qua_produto1 = input("Digite quantos vão ser adicionados: ")
+                        if qua_produto1.isnumeric():
+                            
+                            break
+                        else:
+                            print("\033[31mIsso não é um número !\033[0m")
+
+                    qua_produto = int(qua_produto1)
+
+                    for i in range(qua_produto):
+
+                        carrinho.append(produto_enco)
+                        produto_enco['estoque'] = produto_enco['estoque'] - 1
+
+                    with open('produtos.json', 'w', encoding="utf-8") as dados:
+                        json.dump(produtos, dados, ensure_ascii= False, indent= 4)
+
+                    print("\033[32mProduto adicionado !\033[0m")
+                    print() 
                     break
 
-            if produto_enco is not None:
-                break
+            print("Ainda vai adicionar produtos? ") 
+            print()     
 
-        print(f"Nome: {produto_enco['nome']}")
-        print(f"Preço: {produto_enco['preço']}")
-        print(f"Estoque: {produto_enco['estoque']}")
-        print(f"Código: {produto_enco['codigo']}")
-        print()
-
-        opção = input("O produto está correto : ").lower().strip()
-        if opção in ['sim', 's', 'positivo', 'correto', 'certo', 'ss']:
-
-            carrinho.append(produto_enco)
-
-            print("\033[32mProduto adicionado !\033[0m")
+            continuar = input("Sim ou Não: ").lower().strip()
             print()
+
+            if continuar in ['não', 'n']:
+                break
+            elif continuar in ['sim', 's', 'positivo', 'correto']:
+                print() 
+            else:
+                print("\033[31mOpção inválida !\033[0m")
+                print()
+
+    elif escolha == 2:
+
+        total = sum(produto['preço'] for produto in carrinho)
+
+        print(f"O total a pagar é de R${total}.")
+
+        break
